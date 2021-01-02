404dfc1d27acd9fc149b2d168c5e0f731c28a2d6
shashika nuwan <kumararanaweera1999.com>
2021-01-02 18:33:08 +0530

    config permission package

---
+++ app/Models/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Role
+{
+    const ROLE_SUPER_ADMIN = 'superadmin';
+    const ROLE_ADMIN = 'admin';
+    const ROLE_TEACHER = 'teacher';
+    const ROLE_STUDENT = 'student';
+}
