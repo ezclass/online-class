117c02b03706889a4b5b0bb98238103fb5a30357
shashika nuwan <kumararanaweera1999.com>
2021-02-24 12:38:33 +0530

    modifi validation error msg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,20 @@
 <x-app-layout>
 
-        <!-- About Section -->
-        <section id="about" class="relative py-20 bg-black text-white">
-            <div class="container mx-auto px-4">
-                <div class="items-center flex flex-wrap">
-                    <div class="w-full md:w-5/12 ml-auto mr-auto px-4" data-aos="fade-left">
-                        <div class="md:pr-12">
-                            <small class="text-orange-500">Online Class</small>
-                            <h3 class="text-4xl uppercase font-bold">Online Learning Platform</h3>
-                            <p class="mt-4 text-lg leading-relaxed">
+    <!-- About Section -->
+    <section id="about" class="relative py-20 bg-black text-white">
+        <div class="container mx-auto px-4">
+            <div class="items-center flex flex-wrap">
+                <div class="w-full md:w-5/12 ml-auto mr-auto px-4" data-aos="fade-left">
+                    <div class="md:pr-12">
+                        <small class="text-orange-500">Online Class</small>
+                        <h3 class="text-4xl uppercase font-bold">Online Learning Platform</h3>
+                        <p class="mt-4 text-lg leading-relaxed">
 
-                            </p>
-                        </div>
+                        </p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
+    
 </x-app-layout>
