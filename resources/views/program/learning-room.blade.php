d09193027c964ad6061bf466d68f8fdb483ecb07
shashika nuwan <kumararanaweera1999.com>
2021-02-24 23:35:40 +0530

    create learning room theam

--- resources/views/program/learning-room.blade.php
+++ resources/views/program/learning-room.blade.php
@@ -1,16 +1,7 @@
-<x-app-layout>
-    <div class="mt-10 ml-5">
-        {{$lesson->program->subject->name}} <br>
-        {{$lesson->name}} <br>
-        {{$lesson->program->teacher->name}}
-    </div>
+<x-learning-room>
 
+    {{$lesson->program->subject->name}} <br>
+    {{$lesson->name}} <br>
+    {{$lesson->program->teacher->name}}
 
-<nav class="mt-10 ml-5">
-    <ul>
-        <li> <a href="">Nots</a></li>
-        <li> <a href="">Paper</a></li>
-        <li> <a href="">Vodeo</a></li>
-    </ul>
-</nav>
-</x-app-layout>
+</x-learning-room>
