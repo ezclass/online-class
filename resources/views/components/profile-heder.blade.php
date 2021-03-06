4c1877e57298f228ff3a700f5448671e7ee3f778
shashika nuwan <kumararanaweera1999.com>
2021-03-06 12:56:04 +0530

    add git ignore in storage

--- resources/views/components/profile-heder.blade.php
+++ resources/views/components/profile-heder.blade.php
@@ -1,5 +1,5 @@
 <div class="top h-64 w-full overflow-hidden relative">
-    <img src="{{ asset('storage/dashboard/banner.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
+    <img src="{{ asset('storage/dashboard/banner3.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
     <div class="flex flex-col justify-center items-center relative h-full bg-black bg-opacity-50 text-white">
         <img src="{{ asset('storage/avatar/'. Auth::user()->avatar )}}" class="h-24 w-24 object-cover rounded-full">
         <h1 class="text-2xl font-semibold">{{Auth::user()->name}}</h1>
--- resources/views/program/program-view.blade.php
+++ resources/views/program/program-view.blade.php
@@ -1,30 +1,28 @@
 <x-app-layout>
 
-    <div class="w-full relative mt-1  rounded my-24 overflow-hidden">
-        <div class="top h-64 w-full overflow-hidden relative">
-            <img src="{{ asset('storage/dashboard/banner.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
-            <div class="flex flex-col justify-center items-center relative h-full bg-black bg-opacity-50 text-white">
-                <img src="{{ asset('storage/avatar/'. $program->teacher->avatar )}}" class="h-24 w-24 object-cover rounded-full">
-                <h1 class="text-2xl font-semibold">{{$program->teacher->name}}</h1>
-            </div>
+    <div class="top h-64 w-full overflow-hidden relative">
+        <img src="{{ asset('storage/dashboard/banner3.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
+        <div class="flex flex-col justify-center items-center relative h-full bg-black bg-opacity-50 text-white">
+            <img src="{{ asset('storage/avatar/'. $program->teacher->avatar )}}" class="h-24 w-24 object-cover rounded-full">
+            <h1 class="text-2xl font-semibold">{{$program->teacher->name}}</h1>
         </div>
+    </div>
 
-        <div class="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
-            <div class="px-4 pt-4">
+    <div class="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
+        <div class="px-4 pt-4">
 
-                <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                @can('create', $program)
-                <x-create-lesson :program="$program" />
-                @endcan
+            @can('create', $program)
+            <x-create-lesson :program="$program" />
+            @endcan
 
-                <x-alart />
+            <x-alart />
 
-                @foreach($lessons as $lesson)
-                <x-lesson-card :lesson="$lesson" />
-                @endforeach
+            @foreach($lessons as $lesson)
+            <x-lesson-card :lesson="$lesson" />
+            @endforeach
 
-            </div>
         </div>
     </div>
 
