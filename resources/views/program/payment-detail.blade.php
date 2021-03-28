299dd9d240326c129c9228d5cde78054578fabff
shashika nuwan <kumararanaweera1999@gmail.com>
2021-03-28 23:55:29 +0530

    payment detail @empty msg create

--- resources/views/program/payment-detail.blade.php
+++ resources/views/program/payment-detail.blade.php
@@ -1,15 +1,20 @@
 <x-app-layout>
+    
+<x-dashboard-navigation />
 
     <div class="bg-blue-100">
         <section class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto">
-                <div class="flex flex-col text-center w-full mb-20">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">All Students</h1>
+            <div class="container px-5 py-14 mx-auto">
+                <div class="flex flex-col text-center w-full mb-10">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">All Students</h1>
                 </div>
                 @forelse($enrolments as $enrolment)
                 <x-payment-detail :enrolment="$enrolment" />
                 @empty
-                opps
+                <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 p-3 rounded relative my-6  shadow" role="alert">
+                    <strong class="font-bold">Opps!</strong>
+                    <span class="block sm:inline"> Students are not yet in your class </span>
+                </div>
                 @endforelse
             </div>
         </section>
