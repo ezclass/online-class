d6dac6c6e9d7271ef1438f5788d5109347108e50
shashika nuwan <kumararanaweera1999@gmail.com>
2021-04-12 23:01:48 +0530

    filesystem do create

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'do'),
 
     /*
     |--------------------------------------------------------------------------
@@ -50,6 +50,15 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+        ],
+
+        'do' => [
+            'driver' => 'do',
+            'key' => env('DO_ACCESS_KEY_ID'),
+            'secret' => env('DO_SECRET_ACCESS_KEY'),
+            'region' => env('DO_DEFAULT_REGION'),
+            'bucket' => env('DO_BUCKET'),
+            'endpoint' => env('DO_ENDPOINT'),
         ],
 
     ],
