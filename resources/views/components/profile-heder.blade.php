d7875d4abffa1997ffc30dc274e984fcefef812a
shashika nuwan <kumararanaweera1999@gmail.com>
2021-04-15 00:27:11 +0530

    add avatar and banner

--- resources/views/components/profile-heder.blade.php
+++ resources/views/components/profile-heder.blade.php
@@ -1,7 +1,7 @@
 <div class="top h-64 w-full overflow-hidden relative">
-    <img src="{{ asset('storage/dashboard/banner3.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
+    <img src="{{ Storage::disk('do')->url('dashboard/dashboard.jpg')}}" class="bg w-full h-full object-cover object-center absolute z-0">
     <div class="flex flex-col justify-center items-center relative h-full bg-black bg-opacity-50 text-white">
-        <img src="{{ asset('storage/avatar/'. Auth::user()->avatar )}}" class="h-24 w-24 object-cover rounded-full">
+        <img src="{{ Storage::disk('do')->url('avatar/'. Auth::user()->avatar)}}" class="h-24 w-24 object-cover rounded-full">
         <h1 class="text-2xl font-semibold">{{Auth::user()->name}}</h1>
     </div>
 </div>
