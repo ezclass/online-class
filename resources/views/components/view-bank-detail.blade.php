7fdb848b1caa0ea05530fb8435ab564fec5b2ffc
shashika nuwan <kumararanaweera1999@gmail.com>
2021-04-23 23:14:25 +0530

    chanje bd view

--- resources/views/components/view-bank-detail.blade.php
+++ resources/views/components/view-bank-detail.blade.php
@@ -1,10 +1,21 @@
-{{$bankDetail->name}}
-{{$bankDetail->account_number}}
-{{$bankDetail->branch}}
+<div class="mt-5 mb-32 max-w-4xl p-6 mx-auto bg-gray-200 dark:bg-gray-800 rounded-md shadow-md">
+    <div>
+        <span>Name :</span> {{$bankDetail->name}}
+        <div>
+            <span>Account Number :</span>{{$bankDetail->account_number}}
+        </div>
+        <div>
+            <span>Bank :</span> {{$bankDetail->branch}}
+        </div>
+        <div>
+            <span>Branch :</span> {{$bankDetail->branch}}
+        </div>
+    </div>
 
-<form action="{{route('bank.detail.delete', $bankDetail)}}" method="POST">
-    @csrf
-    <x-danger-button>
-        {{__('Delete')}}
-    </x-danger-button>
-</form>
+    <form action="{{route('bank.detail.delete', $bankDetail)}}" method="POST" class="mt-9">
+        @csrf
+        <x-danger-button>
+            {{__('Delete')}}
+        </x-danger-button>
+    </form>
+</div>
