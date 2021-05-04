1431f558102abfde75a82d6ce5ca74a398788923
shashika nuwan <kumararanaweera1999@gmail.com>
2021-05-04 16:46:56 +0530

    add FORCE_HTTPS in boot file

--- app/Notifications/Otp.php
+++ app/Notifications/Otp.php
@@ -4,7 +4,6 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class Otp extends Notification
@@ -20,13 +19,9 @@ class Otp extends Notification
         return $notifiable->prefers_sms ? ['textit'] : ['mail'];
     }
 
-    public function toMail($notifiable)
+    public function toOtp($notifiable)
     {
-        return (new MailMessage)
-            ->mailer('postmark')
-            ->line('The introduction to the notification.')
-            ->action('Click to go to the site', url('/'))
-            ->line('Thank you for using homeclass.lk!');
+
     }
 
     public function toArray($notifiable)
