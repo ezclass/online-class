0d6b1aaefeeec0b5e24ec25eb8de7a3ca43cda73
shashika nuwan <kumararanaweera1999@gmail.com>
2021-05-15 12:30:57 +0530

    add logo footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,31 +1,7 @@
 <footer class="text-white bg-gray-900 body-font">
     <div class="container px-5 py-8 mx-auto flex items-center md:flex-row flex-col">
         <a href="https://homeclass.lk/" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-600">
-            <svg viewBox="-10 0 550 450" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" class="w-12 h-12  p-2 bg-blue-100 rounded-full" viewBox="0 0 25 25">
-                <defs>
-                    <style>
-                        .cls-1 {
-                            font-size: 313.28px;
-                        }
-
-                        .cls-1,
-                        .cls-3 {
-                            fill: #3953a4;
-                            font-family: EdwardianScriptITC, Edwardian Script ITC;
-                        }
-
-                        .cls-2 {
-                            fill: #6fccdd;
-                        }
-
-                        .cls-3 {
-                            font-size: 59.12px;
-                        }
-                    </style>
-                </defs>
-                <title>HomeClass</title><text class="cls-1" transform="translate(49.93 355.79)">H</text>
-                <path class="cls-2" d="M166.45,70.53C34.08,157.09-30,293.4,23.37,375c34.26,52.39,108.6,69.47,192.47,52.07-60.58,6.8-112.49-9.11-138.06-48.22C32.52,309.62,86.86,194,199.16,120.55s240-76.85,285.28-7.64C510,152,503.78,206,473.26,258.72c49.57-69.86,63.72-144.81,29.47-197.2C449.38-20.06,298.82-16,166.45,70.53Z" transform="translate(-4.57 -2.88)" /><text class="cls-3" transform="matrix(1.17, -0.81, 0.57, 0.82, 213.42, 457.64)">Home Class</text>
-            </svg>
+          <img src="{{Storage::disk('do')->url('logo/logo.png')}}" alt="logo" class="w-12 h-12  p-2 bg-blue-100 rounded-full">
             <span class="ml-3 text-xl text-white">HomeClass</span>
         </a>
         <p class="text-sm text-white sm:ml-4 sm:pl-4 md:border-l-2 sm:border-gray-500 sm:py-2 sm:mt-0 mt-4">© 2021 Made By —
