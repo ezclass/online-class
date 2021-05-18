77667a5a22995c360b11bbdba1f180ed65dcf338
shashika nuwan <kumararanaweera1999@gmail.com>
2021-05-18 20:51:55 +0530

    change term and condition

--- resources/views/pages/term-and-condition.blade.php
+++ resources/views/pages/term-and-condition.blade.php
@@ -1,5 +1,119 @@
 <x-guest-layout>
 
-    <p>Terms & Conditions</p>
+    <nav>
+
+    </nav>
+    <div class="pt-16 p-16">
+        <p class="text-2xl">Zencemart Education Terms & Conditions</p>
+
+        <div class="mt-5">
+            <p>You must agree to terms and conditions before using our Services.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>This User Agreement is an agreement between you and Zencemart Education and applies to your use of HomeClass and any relevant services available through homeclass.lk ("Sites").</p>
+        </div>
+
+        <div class="mt-5">
+            <p>If you don't agree to be bound by the terms and conditions of this Agreement, you will not use this Website or any of the Services.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>Zencemart Education reserves the right to amend this Agreement at any time by posting a revised version on this website. The amendments will be effective from 1-5 (1-5) days after the publication of the amendments. Advertisements about upcoming changes will be posted on our website and on the notification pages posted on your HomeClass account.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>In this agreement, "you" or "yours" means any person ("users") who use the Service.</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-xl">1. ELIGIBILITY</p>
+        </div>
+
+        <div class="mt-5">
+            <p>This service can be used by all teachers who have been appointed as teachers and by any student of school age or student guardian.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>You agree that any information you provide to us ("User Information") will remain in our records until the conclusion of this Agreement.</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-lg">1.1 Warranties</p>
+        </div>
+
+        <div class="mt-5">
+            <p>You represent and warrant that all of the following are true of you from your entry into this Agreement until the termination of your Account</p>
+        </div>
+
+        <div class="mt-5">
+            <p>If you are an teacher, then you are at least 18 years of age and do not require the consent, approbation or approval of any other person or entity to conclude a contractual obligation of the type represented in this Agreement.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>You will refrain from using abusive and vulgar language with Zencemart Education employees, whether through email, blog comments or phone support. Abusive conduct will not be tolerated and will result in account termination.</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-2xl">2. LIABILITY</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-lg">2.1 Our Liability</p>
+        </div>
+
+        <div class="mt-5">
+            <p>We (Zencemart Education) are not responsible for non-payment of tuition fees by students. However, homeclass.lk has the technology to provide advance notice to students who have not paid tuition fees and to block those students if necessary</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-lg">2.2 Your Liability</p>
+        </div>
+
+        <div class="mt-5">
+            <p>Your HomeClass account is protected by a password. As long as you keep the password confidential, no one will be able to access the information in your HomeClass account. </p>
+        </div>
+
+        <div class="mt-5">
+            <p>If a member shares their password with others or allows others to discover and use their password with the account, the account holder is giving access to the information in the account and assumes all responsibility for transactions in this account.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>Please note that all activities performed on your account while logged in will be assumed to be performed by the account holder.</p>
+        </div>
+
+        <div class="mt-5">
+            <p>To use the services available in HomeClass, Please note that any user must be logged in.</p>
+        </div>
+
+        <div class="mt-5">
+            <p class="text-lg">2.3 Teacher Liability</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>If you are not a real teacher, but you are registered as a teacher at homeclass.com your account will be deactivated</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>You will not be charged tuition fees except through Homeclass.lk. If you do, your account will be deactivated.</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>You will not tell students inappropriate suggestions or inappropriate content. If you do, it will cause your account to be canceled.</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>You will not use homeclass.lk for anything other than teaching. If you do, you will no longer be on homeclass.lk.</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>We (Zencemart Education) charge 10% of the fees you charge per student. If you do not like it, you will not use any of the services at homeclass.lk.</p>`
+        </div>
+
+        <div class="mt-5">
+            <p>Regardless of the date you charge students for tuition (provided by homeclass.lk), you agree that we (Zencemart Education) will pay you within the last three days of the month.(Sometimes you will even get paid on the day the students pay). If you do not agree, you will not be able to use any of the services at homeclass.lk.</p>`
+        </div>
+
+    </div>
 
 </x-guest-layout>
