b13bc1e9cd70a5bc02bef78ed34bf8ea16f2a3c5
shashika nuwan <kumararanaweera1999@gmail.com>
2021-06-26 21:35:16 +0530

    create reminder notification and add create class add photo and, show 4n number and email in role student

--- app/Http/Controllers/Enroll/RemindController.php
+++ app/Http/Controllers/Enroll/RemindController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Enroll;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RemindRequest;
 use App\Models\Enrolment;
+use App\Notifications\Reminder;
 
 class RemindController extends Controller
 {
     public function __invoke(RemindRequest $request, Enrolment $enrolment)
     {
         $enrolment->remind($request->get('remind'));
+        $enrolment->student->notify(new Reminder($enrolment));
 
         return redirect()->route('student.detail', $enrolment->program)
             ->with('success', 'Remind Send Successful');
